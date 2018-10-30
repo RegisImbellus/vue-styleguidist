2d1975bad6dd7c6a7262ed558c3550ee9d2b8e71
Regis Frey <rfrey@imbellus.com>
2018-10-29 23:00:05 -0700

    Merge in vue-webpack-loaders to the one place it gets referenced.

--- examples/nuxtjs/styleguide.config.js
+++ examples/nuxtjs/styleguide.config.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path');
-const rules = require('vue-webpack-loaders');
+const vueLoader = require('vue-loader');
+const vueCssLoaders = require('./vue-css-loaders.js');
 
 module.exports = {
 	components: 'src/components/**/[A-Z]*.vue',
@@ -12,7 +13,54 @@ module.exports = {
 			},
 		},
 		module: {
-			rules,
+			rules: [
+				{
+					test: /\.vue$/,
+					exclude: /node_modules/,
+					loader: 'vue-loader',
+					options: {
+						loaders: vueCssLoaders(),
+					}
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+					options: {
+						"presets": [
+							["env", { "modules": false }],
+							"stage-2"
+						],
+						"plugins": ["transform-runtime"],
+						"comments": false,
+						"env": {
+							"test": {
+								"presets": ["env", "stage-2"],
+								"plugins": [ "istanbul" ]
+							}
+						}
+					}
+				},
+				{
+					test: /\.css$/,
+					loader: 'style-loader!css-loader'
+				},
+			
+				{
+					test: /\.json$/,
+					loader: 'json-loader'
+				},
+			
+				{
+					exclude: [ /\.html$/, /\.js$/, /\.css$/, /\.vue$/, /\.json$/ ],
+					loader: 'url-loader',
+					query: {
+						limit: 10000,
+						name: 'static/media/[name].[hash:8].[ext]'
+					}
+				}
+			],
+			plugins: [new vueLoader.VueLoaderPlugin()],
 		},
 	},
 	showUsage: true,
